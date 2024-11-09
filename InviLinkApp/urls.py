f67618c0605696c07b8ac84531a0b91f2a6636fe
Kajetan <kajetanmieloch@gmail.com>
2024-11-09 15:36:05 +0100

    Add responsive navbar and styled event list using Bootstrap
    - Integrated Bootstrap for a responsive and modern navbar.
    - Updated base.html to include a Bootstrap-powered navigation bar.
    - Added event cards to event_list.html for a cleaner and more organized layout.
    - Applied custom CSS to enhance styling of navbar and event cards.

--- InviLinkApp/urls.py
+++ InviLinkApp/urls.py
@@ -2,9 +2,19 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    # Ścieżka do panelu administracyjnego Django
     path('admin/', admin.site.urls),
+
+    # Ścieżka do wydarzeń (appka events)
     path('events/', include('events.urls')),
+
+    # Główna strona aplikacji przekierowuje na listę wydarzeń
     path('', include('events.urls')), 
+
+    # Ścieżki logowania i wylogowania
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
