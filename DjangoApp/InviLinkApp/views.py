3b86e3c1be03c45974583b14d8aad11e95da3af0
Kajetan <kajetanmieloch@gmail.com>
2024-11-16 14:09:35 +0100

    Add publickey to userprofile

--- DjangoApp/InviLinkApp/views.py
+++ DjangoApp/InviLinkApp/views.py
@@ -3,6 +3,10 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import login
 from django.contrib.auth.models import User
+from django.contrib.auth.models import User
+from userProfile.models import UserProfile
+
+
 
 
 @csrf_exempt
@@ -14,6 +18,12 @@ def phantom_login(request):
         # Szukaj użytkownika po publicznym kluczu (adresie Solana) lub utwórz nowego
         user, created = User.objects.get_or_create(username=public_key)
         login(request, user)
+
+        if request.user.is_authenticated:
+            user_profile = UserProfile.objects.get_or_create(user=request.user)[0]
+            user_profile.public_key = public_key
+            user_profile.save()
+
 
         return JsonResponse({"status": "ok"})
     return JsonResponse({"error": "Invalid request"}, status=400)
--- DjangoApp/userProfile/models.py
+++ DjangoApp/userProfile/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     nickname = models.CharField(max_length=100, blank=True, null=True)
+    public_key = models.CharField(max_length=150, blank=True, null=True)
 
     def __str__(self):
         return self.user.username
