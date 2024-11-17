d037cf295830c4d1b36a31b5242a99a3bda14404
Kajetan <kajetanmieloch@gmail.com>
2024-11-17 16:56:37 +0100

    Add basci pahntom wallet integration

--- DjangoApp/blockchain_methods/urls.py
+++ DjangoApp/blockchain_methods/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('initialize/', views.initialize_blockchain, name='initialize_blockchain'),
+    path('send_sol/', views.send_sol, name='send_sol'),
 ]
