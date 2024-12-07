46eca4320c35fa764babcba82f3e007f2857f768
Kajetan <kajetanmieloch@gmail.com>
2024-12-07 15:19:45 +0100

    SMARTCONTRACT INTEGRATION DONE

--- DjangoApp/test_blockchain/urls.py
+++ DjangoApp/test_blockchain/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('phantom_test/', views.phantom_test, name='phantom_test'),
-    path('phantom_callback/', views.phantom_callback, name='phantom_callback'),
-    path('phantom_success/', views.phantom_success, name='phantom_success'),
+    # path('phantom_callback/', views.phantom_callback, name='phantom_callback'),
+    # path('phantom_success/', views.phantom_success, name='phantom_success'),
 ]
