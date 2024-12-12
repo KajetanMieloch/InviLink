ec588550dcb0d7642ffeb1b973aec7b45d301d81
root <root@c385.mikr.us>
2024-12-12 12:41:35 +0000

    Clean code
    -Delete auth_blockchain (legacy mobile login)
    -Delete views in test_blockchain (legacy mobile transaction)

--- DjangoApp/auth_blockchain/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('phantom/login/', views.phantom_mobile_login, name='phantom_mobile_login'),
-    path('phantom/callback/', views.phantom_callback, name='phantom_callback'),
-]
--- DjangoApp/test_blockchain/urls.py
+++ DjangoApp/test_blockchain/urls.py
@@ -4,6 +4,4 @@ from django.urls import path
 urlpatterns = [
     path('phantom_test/', views.phantom_test, name='phantom_test'),
     path('blockchain-data/<str:signature>', views.get_blockchain_data, name='blockchain_data'),
-    # path('phantom_callback/', views.phantom_callback, name='phantom_callback'),
-    # path('phantom_success/', views.phantom_success, name='phantom_success'),
 ]
