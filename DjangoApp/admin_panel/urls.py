654e480e38a35c8dbac4cca2df0da882e1331030
Kajetan <kajetanmieloch@gmail.com>
2025-03-20 15:45:16 +0100

    New landing page. Delete staticfiles and static

--- DjangoApp/admin_panel/urls.py
+++ DjangoApp/admin_panel/urls.py
@@ -1,4 +1,3 @@
-from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
@@ -6,8 +5,5 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('test_blockchain/', include('test_blockchain.urls')),
     path('', views.home, name='home'),
-    path('admin_panel/', include('admin_panel.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
