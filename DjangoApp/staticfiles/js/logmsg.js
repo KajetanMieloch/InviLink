a08a3cf627630965adcdd75cff95fcd1f57245b9
Kajetan <kajetanmieloch@gmail.com>
2025-03-21 21:14:30 +0100

    Log -> logContent

--- DjangoApp/staticfiles/js/logmsg.js
+++ DjangoApp/staticfiles/js/logmsg.js
@@ -1,5 +1,9 @@
 function logMessage(message) {
-    const logEl = document.getElementById("log");
-    logEl.textContent += message + "\n";
-    console.log(message);
+  const logEl = document.getElementById("logContent");
+  if (!logEl) {
+    console.warn("Element o id 'logContent' nie został znaleziony.");
+    return;
   }
+  logEl.textContent += message + "\n";
+  console.log(message);
+}
