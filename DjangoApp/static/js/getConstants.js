2d841d75d287099bb7b13c094e5373303cb304f0
Kajetan <kajetanmieloch@gmail.com>
2025-03-21 20:45:04 +0100

    Repair error that existed due to wrong conversion in public key

--- DjangoApp/static/js/getConstants.js
+++ DjangoApp/static/js/getConstants.js
@@ -1,10 +1,5 @@
 async function getConstants() {
     const res = await fetch('/static/constants/program.json');
-    const data = await res.json();
-  
-    return {
-      PROGRAM_ID: new solanaWeb3.PublicKey(data.PROGRAM_ID),
-      NETWORK: data.NETWORK
-    };
+    return await res.json();
   }
   
