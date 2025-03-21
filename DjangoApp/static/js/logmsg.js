4fc7eba5607df1d783ffa1a5c6d3109a0fc4b6ba
Kajetan <kajetanmieloch@gmail.com>
2025-03-21 21:09:17 +0100

    Log -> logContent

--- DjangoApp/static/js/logmsg.js
+++ DjangoApp/static/js/logmsg.js
@@ -1,5 +1,5 @@
 function logMessage(message) {
-    const logEl = document.getElementById("log");
+    const logEl = document.getElementById("logContent");
     logEl.textContent += message + "\n";
     console.log(message);
   }
