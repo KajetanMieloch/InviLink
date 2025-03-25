4100863b02b03174b5c1439f2293d80c02df67e1
Kajetan <kajetanmieloch@gmail.com>
2025-03-25 17:30:33 +0100

    Add events.html

--- DjangoApp/event_zone/views.py
+++ DjangoApp/event_zone/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "event_zone/home.html")
+
+def events(request):
+    return render(request, "event_zone/events.html")
