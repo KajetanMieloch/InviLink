ae35386a18e86301b46dd21ea9f194b09ef08964
kajetanmieloch <kajetanmieloch@gmail.com>
2025-06-02 20:45:49 +0200

    Add error test  as subsites

--- DjangoApp/InviLinkApp/views.py
+++ DjangoApp/InviLinkApp/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponseBadRequest
+from django.http import HttpResponseServerError
 
 def home(request):
     return render(request, "home.html")
+
+def generate_400(request):
+    return HttpResponseBadRequest("To jest błąd 400: Niepoprawne żądanie")
+
+def generate_500(request):
+    raise Exception("To jest błąd 500: Symulowany wyjątek")
